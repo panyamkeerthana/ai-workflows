df49d165985119011fc0b8f178e732f579d556ed
Siteshwar Vashisht <svashisht@redhat.com>
2025-08-06 12:44:57 +0200

    Remove namespace field from OpenShift configurations
    Signed-off-by: Siteshwar Vashisht <svashisht@redhat.com>

--- beeai/openshift/deploy.sh
+++ beeai/openshift/deploy.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+# TODO: This should be removed when we have more than one OpenShift deployment.
+oc project jotnar-prod
+
 for filename in *.yml; do
     # This check prevents an error if no .yml files are found.
     if [ -e "$filename" ]; then
         echo "Applying $filename ..."
+        # TODO: avoid accidentally applying to wrong namespace by using `-n` argument
+        # https://github.com/packit/ai-workflows/pull/74#discussion_r2256759455
         oc apply -f "$filename"
     fi
 done
