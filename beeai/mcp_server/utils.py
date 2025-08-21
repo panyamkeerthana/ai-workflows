1eea2339e2db578a0609055e4205a131854fbe62
Nikola Forró <nforro@redhat.com>
2025-08-21 16:43:15 +0200

    Fix return value of `init_kerberos_ticket()`
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- beeai/mcp_server/utils.py
+++ beeai/mcp_server/utils.py
@@ -16,7 +16,7 @@ def extract_principal(keytab_file: str) -> str | None:
     """
     proc = subprocess.run(["klist", "-k", "-K", "-e", keytab_file], capture_output=True, text=True)
     print(proc.stdout)
-    if proc.returncode != 0:
+    if proc.returncode:
         print(proc.stderr)
         return None
     key_pattern = re.compile(r"^\s*(\d+)\s+(\S+)\s+\((\S+)\)\s+\((\S+)\)$")
@@ -39,10 +39,10 @@ def init_kerberos_ticket() -> bool:
         return False
     proc = subprocess.run(["klist", "-l"], capture_output=True, text=True)
     print(proc.stdout)
-    if proc.returncode != 0:
+    if proc.returncode:
         print(proc.stderr)
     elif principal in proc.stdout:
         return True
     env = os.environ.copy()
     env.update({"KRB5_TRACE": "/dev/stdout"})
-    return subprocess.run(["kinit", "-k", "-t", keytab_file, principal], env=env) == 0
+    return not subprocess.run(["kinit", "-k", "-t", keytab_file, principal], env=env).returncode
