0d0bcdf13b307a4488a7e240d3ba4e0d1422792c
Tomas Tomecek <ttomecek@redhat.com>
2025-10-08 11:16:14 +0200

    quick-deply: pull latest bee images before deploy

--- openshift/quick-deploy.sh
+++ openshift/quick-deploy.sh
@@ -4,6 +4,8 @@ set -e
 
 oc project jotnar-prod
 
+oc import-image beeai-agent --all
+oc import-image mcp-server --all
 oc apply -n jotnar-prod -f deployment-backport-agent-c10s.yml
 oc apply -n jotnar-prod -f deployment-backport-agent-c9s.yml
 oc apply -n jotnar-prod -f deployment-rebase-agent-c10s.yml
